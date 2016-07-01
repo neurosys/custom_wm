3bd5d66eec5a97073e983a899f3752e9081599d1
NeuroSys <bla-bla>
2016-07-01 08:17:49 +0300

    Added fibonacci layout

--- config.h
+++ config.h
@@ -46,6 +46,8 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
  	{ "###",      horizgrid },
+  	{ "[@]",      spiral },
+  	{ "[\\]",     dwindle },
 };
 
 /* key definitions */
@@ -85,6 +87,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,                setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_m,                setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_e,                setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_s,                setlayout,      {.v = &layouts[4]} },
+	{ MODKEY,                       XK_a,                setlayout,      {.v = &layouts[5]} },
 	{ MODKEY,                       XK_space,            setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,            togglefloating, {0} },
 	{ MODKEY,                       XK_0,                view,           {.ui = ~0 } },
